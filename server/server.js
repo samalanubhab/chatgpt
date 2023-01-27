f105a5d3ae48ad28a884c6931a3a665efa280615
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-28 02:13:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
 import xlsx from 'xlsx'
-import {cosineSimilarity} from './cosineSimilarity.js'
+import math from 'mathjs'
 
 dotenv.config()
 
@@ -33,26 +33,17 @@ app.post('/', async (req, res) => {
         });
         const queryEmbedding = output.data.embedding;
         
-
-                
         const workbook = xlsx.readFile('./document_embeddings.xlsx');
         const sheet = workbook.Sheets[workbook.SheetNames[0]];
         const data = xlsx.utils.sheet_to_json(sheet);
-        console.log(data);
-        
-        let similarities = [];
-        data.forEach(d => {
-            const documentEmbedding = d.embeddings
-            const similarity = cosineSimilarity(queryEmbedding, documentEmbedding)
-            similarities.push({
-                similarity,
-                context: d.context
-            })
-        });      
-        similarities.sort((a, b) => b.similarity - a.similarity);
-        
-        const topMatchingContext = similarities[0].context;
-        const prompt1 = topMatchingContext;
+        data.forEach(row => {
+          row.similarity = math.dot(queryEmbedding, row.embeddings);
+        });
+        data.sort((a, b) => b.similarity - a.similarity);
+              
+
+        const prompt1 = data[0].context;
+        console.log(prompt1);
         
       
         const newPrompt = `Context: ${prompt1}\n Question: ${query}`;
