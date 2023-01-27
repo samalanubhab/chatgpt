706e8f0fab2b6339210607f6ae08d845790d36b0
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-27 22:58:32 +0530

    Create cosineSimilarity.js

---
+++ server/cosineSimilarity.js
@@ -0,0 +1,25 @@
+exports.cosineSimilarity = function(v1, v2) {
+    // Check if vectors have the same length
+    if (v1.length !== v2.length) {
+        throw new Error('Vectors must have the same length');
+    }
+
+    // Calculate dot product
+    let dotProduct = 0;
+    for (let i = 0; i < v1.length; i++) {
+        dotProduct += v1[i] * v2[i];
+    }
+
+    // Calculate magnitudes
+    let v1Magnitude = 0;
+    let v2Magnitude = 0;
+    for (let i = 0; i < v1.length; i++) {
+        v1Magnitude += v1[i] * v1[i];
+        v2Magnitude += v2[i] * v2[i];
+    }
+    v1Magnitude = Math.sqrt(v1Magnitude);
+    v2Magnitude = Math.sqrt(v2Magnitude);
+
+    // Calculate cosine similarity
+    return dotProduct / (v1Magnitude * v2Magnitude);
+}
