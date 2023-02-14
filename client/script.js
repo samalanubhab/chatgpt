c76cbf006868175c16bb26dfe81147f5ceeb18e4
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-02-14 20:42:43 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgptai-83tg.onrender.com', {
+    const response = await fetch('https:onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
